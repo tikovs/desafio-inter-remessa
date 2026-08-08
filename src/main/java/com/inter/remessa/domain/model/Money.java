2c7e37370b0ece1eaaaf1438db80682ee979e443
Thiago <thiago@Laptop-de-Thiago.local>
2026-08-08 20:46:38 -0300

    feat: implementa método de converter reais em centavos na classe  Money

--- src/main/java/com/inter/remessa/domain/model/Money.java
+++ src/main/java/com/inter/remessa/domain/model/Money.java
@@ -5,4 +5,8 @@ import java.math.RoundingMode;
 
 public record Money(long cents) {
 
+    public static Money ofReais(BigDecimal reais) {
+        long centavos = reais.movePointRight(2).setScale(0, RoundingMode.HALF_EVEN).longValueExact();
+        return new Money(centavos);
+    }
 }
